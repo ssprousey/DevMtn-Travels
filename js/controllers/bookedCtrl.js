d817e0ff33b349900c38ff2667c2f345e2b63262
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-06-17 11:50:22 -0600

    finished

--- js/controllers/bookedCtrl.js
+++ js/controllers/bookedCtrl.js
@@ -1,3 +1,21 @@
 angular.module('devmtnTravel')
-.controller('packagesCtrl', function($scope, $stateParams, mainService) {
-  var booked = $stateParams.country;
+.controller('bookedCtrl', function($scope, $stateParams, mainService) {
+  var booked = $stateParams.id;
+
+  if (booked) {
+
+    // $scope.packageInfo = mainService.packageInfo.filter(function(package) {
+    //
+    //   return package.id == booked;
+    // })
+
+    mainService.packageInfo.forEach(function(package) {
+      if (package.id == booked) {
+        $scope.booked = package;
+      }
+    })
+
+
+
+  }
+});
